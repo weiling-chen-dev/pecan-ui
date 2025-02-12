f23600558b5a2d9d497bf04aaf1db0d2bb6bcee3
Wei-Ling <weiling.chen@funpodium.net>
2025-02-12 11:29:36 +0800

    update package version

--- lib/Button/Button.test.tsx
+++ lib/Button/Button.test.tsx
@@ -5,6 +5,15 @@ describe(`Component: ${Button.name}`, () => {
   it("should render", () => {
     const { container } = render(<Button>My button</Button>);
 
-    expect(container).toMatchInlineSnapshot();
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          class="group btn py-[3px] px-[10px] rounded-md bg-white hover:bg-white text-default-dark hover:text-primary-500 border-1 border-solid border-default-light hover:border-primary-500"
+          type="button"
+        >
+          My button
+        </button>
+      </div>
+    `);
   });
 });
